6ee80b2ba9622154d3ae40bbba19bf56f24b373a
Van Luu <valu4298@gmail.com>
2016-06-30 22:58:07 +0700

    custom Playground
     - add shape
     - add physic

--- SpriteKitPhysicsTest.playground/Contents.swift
+++ SpriteKitPhysicsTest.playground/Contents.swift
@@ -1,5 +1,37 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import SpriteKit
+import XCPlayground
 
-var str = "Hello, playground"
+let sceneView = SKView(frame: CGRect(x: 0, y: 0, width: 480, height: 320))
+let scene = SKScene(size: CGSize(width: 480, height: 320))
+scene.physicsBody = SKPhysicsBody(edgeLoopFromRect: scene.frame)
+sceneView.showsFPS = true
+sceneView.presentScene(scene)
+scene.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+
+XCPShowView("My View", view: sceneView)
+
+let square = SKSpriteNode(imageNamed: "square")
+square.name = "shape"
+square.position = CGPoint(x: scene.size.width * 0.25, y: scene.size.height * 0.5)
+
+let circle = SKSpriteNode(imageNamed: "circle")
+circle.name = "shape"
+circle.position = CGPoint(x: scene.size.width * 0.5, y: scene.size.height * 0.5)
+
+let triangle = SKSpriteNode(imageNamed: "triangle")
+triangle.name = "triangle"
+triangle.position = CGPoint(x: scene.size.width * 0.75, y: scene.size.height * 0.5)
+
+scene.addChild(square)
+scene.addChild(circle)
+scene.addChild(triangle)
+
+circle.physicsBody = SKPhysicsBody(circleOfRadius: circle.size.width / 2)
+
+delay(seconds: 2.0, completion: {
+  scene.physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
+
+})
---
+++ SpriteKitPhysicsTest.playground/Sources/SupportCode.swift
@@ -0,0 +1,9 @@
+import UIKit
+public func delay(seconds seconds: Double, completion:()->()) {
+  let popTime = dispatch_time(DISPATCH_TIME_NOW,
+                              Int64( Double(NSEC_PER_SEC) * seconds ))
+  dispatch_after(popTime,
+                 dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0)) {
+                  completion()
+  }
+}
